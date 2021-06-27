547b7cb4e8e5acc46b643caa8202fa525b30629c
Arnab Mondal <78680046+ArnabMdev@users.noreply.github.com>
2021-06-27 14:52:05 +0530

    final commit

--- index.js
+++ index.js
@@ -1,44 +1,31 @@
 // getting weather data
 let weather = {
   apiKey: "089591cfe82d462915f75705c026a906",
-  fetchWeather: function (city) {
-    fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
-        city +
-        "&units=metric&appid=" +
-        this.apiKey
-    )
+  getWeather: function (city) {
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.apiKey)
       .then((response) => {
-        if (!response.ok) {
-          alert("No weather found.");
-          throw new Error("No weather found.");
-        }
         return response.json();
       })
-      .then((data) => this.displayWeather(data));
+      .then((data) => this.showWeather(data));
   },
 //   displaying weather
-  displayWeather: function (data) {
+  showweather: function (data) {
     const { name } = data;
     const { icon, description } = data.weather[0];
     const { temp, humidity } = data.main;
     const { speed } = data.wind;
     document.querySelector(".city").innerText = "Weather in " + name;
-    document.querySelector(".icon").src =
-      "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
     document.querySelector(".description").innerText = description;
     document.querySelector(".temp").innerText = temp + "°C";
-    document.querySelector(".humidity").innerText =
-      "Humidity: " + humidity + "%";
-    document.querySelector(".wind").innerText =
-      "Wind speed: " + speed + " km/h";
+    document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText = "Wind speed: " + speed + " km/h";
     document.querySelector(".weather").classList.remove("loading");
-    document.body.style.backgroundImage =
-      "url('https://source.unsplash.com/1600x900/?" + description + "')";
+    document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + description + "')";
   },
 //   getting location
   search: function () {
-    this.fetchWeather(document.querySelector(".search-bar").value);
+    this.getWeather(document.querySelector(".search-bar").value);
   },
 };
 
@@ -54,4 +41,4 @@ document
     }
   });
 // calling initial display data
-weather.fetchWeather("Mumbai");
+weather.getWeather("Mumbai");
